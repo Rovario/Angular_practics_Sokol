90f98315facb1782943a3f9bcf2203fabc5e6c3a
Rovario <roman.sokol007@gmail.com>
2025-08-19 05:28:15 +0300

    Initial commit: структура практичних завдань

---
+++ my-first-app/src/app/greeting/greeting.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.html',
+  styleUrl: './greeting.css'
+})
+export class Greeting {
+
+}
+
+
+export class GreetingComponent {
+name = 'студент';
+} 
